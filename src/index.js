2cd2e94f84c02360e80652f119cec8f33b25aca6
Piotr Milewski <milewski.piotr.m@gmail.com>
2018-06-15 17:21:21 +0200

    VideoDetail wip

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyCUw5s2O247IcP5AY5a5qIaRo8TqG1pVP8';
 
 class App extends Component {
@@ -10,7 +11,7 @@ class App extends Component {
         super(props);
         this.state = { videos: [] };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, videos => {
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
             this.setState({ videos });
             // this.setState({ videos: videos }); when state and var have same names
         });
@@ -20,7 +21,8 @@ class App extends Component {
         return (
         <div>
             <SearchBar />
-            <VideoList />
+            <VideoDetail video={this.state.videos[0]} />
+            <VideoList videos={ this.state.videos }/>
         </div>
         );
     }
