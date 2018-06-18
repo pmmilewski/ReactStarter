d517b360ad6d91a1d85a671f36e081db07263a15
Piotr Milewski <milewski.piotr.m@gmail.com>
2018-06-18 14:48:40 +0200

    Finished react app doing youtube search, deleted API key from code

--- src/index.js
+++ src/index.js
@@ -1,28 +1,41 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyCUw5s2O247IcP5AY5a5qIaRo8TqG1pVP8';
+const API_KEY = 'GOOGLE API KEY GOES HERE';
 
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { videos: [] };
+        this.state = {
+             videos: [],
+             selectedVideo: null
+         };
 
-        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
-            this.setState({ videos });
-            // this.setState({ videos: videos }); when state and var have same names
-        });
+         this.videoSearch('surfboards');
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term}, (videos) => {
+            this.setState({
+                 videos: videos ,
+                 selectedVideo: videos[0]
+                });        
+            });
     }
 
     render() {
+        const videoSearch = _.debounce((term) => {this.videoSearch(term)}, 300);
         return (
         <div>
-            <SearchBar />
-            <VideoDetail video={this.state.videos[0]} />
-            <VideoList videos={ this.state.videos }/>
+            <SearchBar onSearchTermChange={videoSearch} />
+            <VideoDetail video={this.state.selectedVideo} />
+            <VideoList
+             onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+             videos={ this.state.videos }/>
         </div>
         );
     }
